86b48fef0ed088e40388c989f5100b13f0b071f5
AnVlMironov <AnVlMironov@Greenatom.ru>
2024-06-30 17:11:48 +0400

    init client app

---
+++ src/clientApp/src/Models/IBookcase.ts
@@ -0,0 +1,6 @@
+export interface IBookCase {
+    number: number;
+    name: string;
+    maxSizeShelve: number;
+    id: string;
+}
--- src/clientApp/vite.config.ts
+++ src/clientApp/vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const proxyTarget: string = 'http://localhost:5272';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        math: 'always',
+        relativeUrls: true,
+        javascriptEnabled: true,
+        mode: 'local'
+      }
+    },
+    modules: {}
+  },
+  server: {
+    //settings dev local server
+    host: 'localhost',
+    port: 3000,
+    https: false,
+    open: true,
+
+    // settings proxy server
+    proxy: {
+      '/api': {
+        target: proxyTarget,
+        changeOrigin: true,
+        cookieDomainRewrite: 'localhost',
+        secure: false
+      },
+    }
+  }
 })
